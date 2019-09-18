04d187be9d66e3a0cb0de938a21bed6473f678a7
mmbouchoucha <mehdiboch@gmail.com>
2019-09-18 12:41:25 +0000

    C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print string using put
+ * Return:  0 to exit
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - to print the string
+ * Return: 0 to exit
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
