72723db33e603c3850f6015776d8d6e704d49230
mmbouchoucha <mehdiboch@gmail.com>
2019-10-01 13:06:03 +0000

    point

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
+
 /**
-* reset_to_98 - update value to 98 via pointer
-* 
-*
-* Return: void
-*/
+ * reset_to_98 - update value to 98 via pointer
+ * @n: pointer
+ *
+ * Return: void
+ */
+
 void reset_to_98(int *n)
 {
 *n = 98;
