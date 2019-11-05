968d3d5bf325ea24d308e35858c517550f7e5d56
mmbouchoucha <mehdiboch@gmail.com>
2019-11-05 22:00:55 +0000

    upex1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * listint_len - determine the num of elem in a linked listint_t list
+ * @h: p of const listint_t type
+ *
+ * Return: num of elem
+ */
+size_t listint_len(const listint_t *h)
+{
+  int i;
+
+  i = 0;
+  if (h)
+    {
+      while (h->next)
+	{
+	  h = h->next;
+	  i++;
+	}
+      i++;
+    }
+  return (i);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the begin of a listint_t list
+ * @head: p to a p of type listint_t
+ * @n: int being passed
+ *
+ * Return: NULL if fail, otherwise p to new elem
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+  listint_t *new;
+  int i;
+
+  new = malloc(sizeof(listint_t));
+
+  if (new == NULL)
+    return (NULL);
+
+  i = n;
+  new->n = i;
+  new->next = *head;
+  *head = new;
+  return (*head);
+}
