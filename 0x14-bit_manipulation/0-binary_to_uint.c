5a7916c26a75f73328a6f3585a066c5293ef7d18
mmbouchoucha <mehdiboch@gmail.com>
2019-11-29 20:50:42 +0000

    update03

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,22 +6,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int r = 0;
-  unsigned int len = 0, i;
+unsigned int r = 0;
+unsigned int len = 0, i;
 
-  if (b == NULL)
-    return (0);
-  while (b[len])
-    {
-      if (b[len] != '0' && b[len] != '1')
-	return (0);
-      len++;
-    }
-  len--;
-  for (i = 0; b[i]; i++, len--)
-    {
-      if (b[i] == '1')
-	r += 1 << len;
-    }
-  return (r);
+if (b == NULL)
+return (0);
+while (b[len])
+{
+if (b[len] != '0' && b[len] != '1')
+return (0);
+len++;
+}
+len--;
+for (i = 0; b[i]; i++, len--)
+{
+if (b[i] == '1')
+r += 1 << len;
+}
+return (r);
 }
