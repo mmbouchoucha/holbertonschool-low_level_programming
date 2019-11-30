84052bb1e1ae154c481ce0d6d6aaaba273ad5e4c
mmbouchoucha <mehdiboch@gmail.com>
2019-11-30 00:25:02 +0000

    up01

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * read_textfile - read text file and print it
+ * read_textfile - read text and print
  * @filename: filename
  * @letters: num of letters to read and print
  *
@@ -9,40 +9,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd;
-  ssize_t rf, wf, cf;
-  char *buff;
+int fd;
+ssize_t rf, wf, cf;
+char *buff;
 
-  if (filename == NULL)
-    return (0);
-  buff = malloc(sizeof(char) * letters);
-  if (buff == NULL)
-    return (0);
-  fd = open(filename, O_RDONLY);
-  if (fd == -1)
-    {
-      free(buff);
-      return (0);
-    }
-  rf = read(fd, buff, letters);
-  if (rf == -1)
-    {
-      free(buff);
-      return (0);
-    }
-  if (rf > 0)
-    wf = write(STDOUT_FILENO, buff, rf);
-  if (wf == -1)
-    {
-      free(buff);
-      return (0);
-    }
-  cf = close(fd);
-  if (cf == -1)
-    {
-      free(buff);
-      return (0);
-    }
-  free(buff);
-  return (rf);
+if (filename == NULL)
+return (0);
+buff = malloc(sizeof(char) * letters);
+if (buff == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+free(buff);
+return (0);
+}
+rf = read(fd, buff, letters);
+if (rf == -1)
+{
+free(buff);
+return (0);
+}
+if (rf > 0)
+wf = write(STDOUT_FILENO, buff, rf);
+if (wf == -1)
+{
+free(buff);
+return (0);
+}
+cf = close(fd);
+if (cf == -1)
+{
+free(buff);
+return (0);
+}
+free(buff);
+return (rf);
 }
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * append_text_to_file - text at the end
+ * @filename: filename.
+ * @text_content: added content.
+ * Return: 1 in file exists, else -1 in fails does not exist
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int yoyo;
+int len;
+int x;
+if (!filename)
+return (-1);
+ala = open(filename, O_WRONLY | O_APPEND);
+if (yoyo == -1)
+return (-1);
+if (text_content)
+{
+for (len = 0; text_content[len]; len++)
+;
+x = write(yoyo, text_content, len);
+if (x == -1)
+return (-1);
+}
+close(yoyo);
+return (1);
+}
